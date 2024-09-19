8427ea5c174b0db545cecc14e1b32d2e0d515d7d
ClOVER-QwQ <1970619599@qq.com>
2024-09-19 09:41:37 +0800

    ClOVER(项目规格)

--- PreDate/src/main/java/PreDate.java
+++ PreDate/src/main/java/PreDate.java
@@ -1,25 +1,6 @@
 import java.util.Scanner;
 
 public class PreDate {
-    public static void main(String[] args) {
-        int year=2024,month=9,day=12,isEnd = 0;
-            Scanner in = new Scanner(System.in);
-        while (isEnd != 1){
-            System.out.println("请依次输入年月份");
-            try {
-                year = in.nextInt();
-                month = in.nextInt();
-                day = in.nextInt();
-            } catch (Exception e) {
-                System.out.println("请输入有效数字");
-                System.exit(0);
-            }
-            System.out.println(preDate(year, month, day));
-            System.out.println("结束请输入1!");
-            isEnd = in.nextInt();
-        }
-    }
-
     public static String preDate(int year, int month, int day) {
         String result;
         if (validateDate(year, month, day)) {
